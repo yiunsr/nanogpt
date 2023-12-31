dc3b76415de28d308545234afc8927d5997ea91f
Yong <yiunsr@hanmail.net>
2023-12-31 20:45:18 +0900

    Update train.py

--- train.py
+++ train.py
@@ -35,15 +35,15 @@ init_from = 'scratch' # 'scratch' or 'resume'
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
 out_dir = r'/content/drive/MyDrive/colab/res/out'
-eval_interval = 500
+eval_interval = 200
 log_interval = 1
-eval_iters = 200
+eval_iters = 100
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
 
 # data
 gradient_accumulation_steps = 5 * 8 # used to simulate larger batch sizes
-batch_size = 12*12 # 12 # if gradient_accumulation_steps > 1, this is the micro-batch size
+batch_size = 350 # 12 # if gradient_accumulation_steps > 1, this is the micro-batch size
 block_size = 256 # 1024
 # model
 n_layer = 6 # 12
