4b05c1a63cb994fc70691daee41a64d82abce5b1
Yong <yiunsr@hanmail.net>
2023-12-31 21:47:28 +0900

    Update train.py

--- train.py
+++ train.py
@@ -92,7 +92,7 @@ ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torc
 ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
 
 # poor man's data loader
-data_dir = "/content/drive/MyDrive/colab/res/"
+data_dir = "/content/"
 train_data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
 val_data = np.memmap(os.path.join(data_dir, 'val.bin'), dtype=np.uint16, mode='r')
 def get_batch(split):
